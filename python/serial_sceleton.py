a84cdbd0df6dbcef47a5532ad0dd85f47dd5feaf
Brandon <20436386@sun.ac.za>
2021-01-20 14:59:20 +0200

    Added code to take in two command line arguments. The first specifys the sampling duration and the second specifys the sampling period. The sampled data is then saved in a .csv log file.

--- python/serial_sceleton.py
+++ python/serial_sceleton.py
@@ -1,21 +1,59 @@
 #!/usr/bin/python3
+#first commandline argument should be the sampling time and the second should be the sampling period
+import sys
 import serial
 import time
+import csv
 
+sampling_time = float(sys.argv[1]) 
+sampling_period = float(sys.argv[2])
 dmm = None
-dmm = serial.Serial(port="/dev/ttyS0", baudrate=115200, timeout=0.005)
+dmm = serial.Serial(port="/dev/ttyS0", baudrate=115200, timeout=sampling_period) #was 0.005
+log_num = 0
+
+#with open("log0" + str(log_num) + ".csv", "w+") as log_file:
+#    log_writer = csv.writer(log_file, delimiter = ',')
+
+current_time = 0
+
+#def _readline(self):
+#    eol = b'\r'
+#    leneol = len(eol)
+#    line = bytearray()
+#    while True:
+#        c = self.ser.read(1)
+#        if c:
+#            line += c
+#            if line[-leneol:] == eol:
+#                break
+#        else:
+#            break
+#    return bytes(line)
 
 def report(dmm):
     if dmm:
         dmm.write(b"read? buf\r")
-        line = dmm.readline().decode("utf-8")
-        # print(f"RX: {line}")
-        
-        reply_list = line.split(',')
+
+        rx_line = dmm.readline().decode("utf-8")
+        #print(f"RX: {char}")
+        reply_list = rx_line.split(',')
         str_value = reply_list[0].lower()
-        # print(f"{str_value}")
+        #print(f"{str_value}")
         return float(str_value)
 
-while(True):
-    print(f"Value: {report(dmm)}")
-    time.sleep(0.005)
+start_time = time.clock_gettime(time.CLOCK_BOOTTIME)
+current_time = (time.clock_gettime(time.CLOCK_BOOTTIME)) - start_time
+
+while(current_time <= sampling_time):
+    #print(f"Value: {report(dmm)}")
+    #print(f"current_time = ", current_time)
+    line = [current_time, report(dmm)]
+
+    with open("log0" + str(log_num) + ".csv", "a") as log_file:
+        log_writer = csv.writer(log_file,dialect='excel' )#,quoting= csv.QUOTE_NONNUMERIC)
+        log_writer.writerow(line)
+
+    current_time = (time.clock_gettime(time.CLOCK_BOOTTIME)) - start_time
+    time.sleep(sampling_period)
+
+
